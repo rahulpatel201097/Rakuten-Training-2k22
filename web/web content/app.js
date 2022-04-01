b98b77bb5d93060f39e5b7e440d03c8645682606
rahulpatel201097 <60782538+rahulpatel201097@users.noreply.github.com>
2022-04-01 16:05:16 +0530

    Add files via upload

---
+++ web/web content/app.js
@@ -0,0 +1,7 @@
+function save(){
+	const element = document.getElementById('age');
+	console.log(element.value);
+	
+	const elements = document.getElementsByTagName('input');
+	console.log(elements);
+}

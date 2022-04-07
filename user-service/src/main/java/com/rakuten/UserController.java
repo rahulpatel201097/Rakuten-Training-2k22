f665e3239fe46d834389e19f1d2abe66f87b34c8
rahulpatel201097 <60782538+rahulpatel201097@users.noreply.github.com>
2022-04-07 10:37:20 +0530

    Add files via upload

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -1,8 +1,6 @@
 package com.rakuten;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,40 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
-	UserService service;
-
-	List<User> users = new ArrayList<>();
+	UserService service; // only reference
 
 	@PostMapping
 	void saveUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
-		users.add(user);
 		service.save(user);
 	}
 
 	@GetMapping // localhost:8081/user
 	List<User> getUsers() {
 		return service.getAllUsers();
-		//return users;
 	}
 
+//
 	@GetMapping("/{name}")
 	List<User> getUsersByName(@PathVariable String name) {
-//		??
 		System.out.println(name);
 		return service.getUserByName(name);
-		//List<User> filteredUsers = users.stream().filter((user) -> user.getName().equals(name))
-		//		.collect(Collectors.toList());
-		//return filteredUsers;
 	}
 
 	@GetMapping("/age/{age}") // localhost:8081/user/age/20
 	List<User> getUsersByage(@PathVariable int age) {
-		if (age <= 0) {
-			throw new IllegalArgumentException("age cannot be negative=" + age);
-		}
-		List<User> filteredUsers = users.stream().filter((user) -> user.getAge() == age).collect(Collectors.toList());
+
+		List<User> filteredUsers = service.getUserByAge(age);
 		return filteredUsers;
 	}
 }

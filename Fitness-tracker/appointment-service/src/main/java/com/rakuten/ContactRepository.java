d1b7100f324997661bfac0c51e8078da694321d2
rahulpatel201097 <60782538+rahulpatel201097@users.noreply.github.com>
2022-05-05 10:18:35 +0530

    Delete

--- Fitness-tracker/appointment-service/src/main/java/com/rakuten/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rakuten;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rakuten.model.ContactMessage;
-
-public interface ContactRepository extends JpaRepository<ContactMessage, Integer> {
-
-}
